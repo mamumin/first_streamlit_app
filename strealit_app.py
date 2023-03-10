8b83469aa7debcca52af1ab04996d25eac9474b3
Malik <119707096+mamumin@users.noreply.github.com>
2023-03-10 09:26:46 -0500

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -22,13 +22,6 @@ streamlit.dataframe(fruit_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
 import requests 
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #take the jason version of response and normalize it
